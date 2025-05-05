6df42eb2ba082dda0ab468090f1ec443f1586677
nayaracalenzo <queiroz.14@hotmail.com>
2025-05-05 16:53:53 -0300

    add objetos

---
+++ index.js
@@ -0,0 +1,46 @@
+const searchInput = document.getElementById('searchInput')
+
+const produtos = [
+    {
+        img: '/assets/livro1.png',
+        titulo: 'Ponto de Inflexão',
+        categoria: 'Desenvolvimento Pessoal',
+        preco: 69.99,
+        desconto: true
+    },
+    {
+        img: '/assets/livro2.png',
+        titulo: 'O poder do aprendizado contínuo',
+        categoria: 'Desenvolvimento Pessoal',
+        preco: 69.99,
+        desconto: true
+    },
+    {
+        img: '/assets/livro3.png',
+        titulo: 'Mais esperto que o Diabo 2',
+        categoria: 'Desenvolvimento Pessoal',
+        preco: 69.99,
+        desconto: false
+    },
+    {
+        img: '/assets/livro4.png',
+        titulo: 'O que se come no céu',
+        categoria: 'Desenvolvimento Pessoal',
+        preco: 69.99,
+        desconto: true
+    },
+    {
+        img: '/assets/livro1.png',
+        titulo: 'Psicologia Financeira',
+        categoria: 'Desenvolvimento Pessoal',
+        preco: 69.99,
+        desconto: true
+    },
+    {
+        img: '/assets/livro1.png',
+        titulo: 'A meta',
+        categoria: 'Desenvolvimento Pessoal',
+        preco: 69.99,
+        desconto: false
+    }
+]
