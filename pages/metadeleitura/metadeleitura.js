978af04893c58390de4d1c1fe980f36a68addfd5
nayaracalenzo <queiroz.14@hotmail.com>
2025-05-07 16:35:17 -0300

    adicionando livros na meta de leitura

--- pages/metadeleitura/metadeleitura.js
+++ pages/metadeleitura/metadeleitura.js
@@ -12,11 +12,9 @@ searchButton.addEventListener('click', (event) => {
 })
 
 function fetchBooks(query) {
-    console.log('1')
     fetch(`https://openlibrary.org/search.json?q=${encodeURIComponent(query)}&language=por`)
     .then(res => res.json())
     .then(dados => {
-        console.log('2')
         const livrosFiltrados = dados.docs.filter(item => 
         item.language.includes('por') && !item.language.includes('eng')
         ).slice(0,10);
@@ -39,7 +37,7 @@ function fetchBooks(query) {
                         <h1 class="truncate text-xs font-bold w-full">${item.title}</h1>
                     </div>
                     <h2 class="my-2">${item.author_name}</h2>
-                    <button class="rounded-md flex justify-center w-full h-6 p-1 border border-blue-500">
+                    <button onclick="adicionarLivro('${urlImagem}')" class="rounded-md flex justify-center w-full h-6 p-1 border border-blue-500">
                     <i class="text-blue-500 fa-solid fa-plus"></i>
                     </button>
                 </div>
@@ -51,7 +49,16 @@ function fetchBooks(query) {
         console.log('Erro ao buscar livros:', erro)
         booksContainer.innerHTML = '<p>Erro ao buscar livros</p>'
     })
-    console.log('3')
+}
+
+function adicionarLivro (capa) {
+    metaLivros.innerHTML += `
+        <div class="div-card w-[60px]">
+            <div class="div-img">
+                <img src='${capa}' alt="capa do livro" class="w-[150px] h-auto">
+            </div>
+        </div> 
+    ` 
 }
 
 
