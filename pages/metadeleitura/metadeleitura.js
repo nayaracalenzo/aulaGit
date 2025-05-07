36dc5f17216eccda81ae6be350dc623f0187b115
nayaracalenzo <queiroz.14@hotmail.com>
2025-05-07 15:16:17 -0300

    fetch API open library

---
+++ pages/metadeleitura/metadeleitura.js
@@ -0,0 +1,53 @@
+const searchInput = document.getElementById('searchInput')
+const searchButton = document.getElementById('searchButton')
+const booksContainer = document.getElementById('booksContainer')
+
+searchButton.addEventListener('click', (event) => {
+    event.preventDefault()
+    const query = searchInput.value.toLowerCase().trim();
+    if (!query) return;
+    fetchBooks(query)
+
+})
+
+function fetchBooks(query) {
+    console.log('1')
+    fetch(`https://openlibrary.org/search.json?q=${encodeURIComponent(query)}&language=por`)
+    console.log('2')
+    .then(res => res.json())
+    .then(dados => {
+        const livrosFiltrados = dados.docs.filter(item => 
+        item.language.includes('por') && !item.language.includes('eng')
+        ).slice(0,10);
+
+        if (livrosFiltrados.length === 0) {
+            booksContainer.innerHTML = '<h1>Nenhum livro encontrado</h1>'
+            return
+        }
+        
+        booksContainer.innerHTML = livrosFiltrados.map(item => {
+            const urlImagem = `https://covers.openlibrary.org/b/id/${item.cover_i}-M.jpg`
+        return `
+            <div class="div-card">
+                <div class="div-img">
+                    <img src='${urlImagem}' alt="capa do livro">
+                    <p class="tag-p">frete grátis</p>
+                </div>
+                <div class="div-texto">
+                    <div class="div-titulo">
+                        <h1>${item.title}</h1>
+                    </div>
+                    <h2>${item.author_name}</h2>
+                </div>
+            </div>
+        
+        `}).join('')
+    })
+    .catch( erro => {
+        console.log('Erro ao buscar livros:', erro)
+        booksContainer.innerHTML = '<p>Erro ao buscar livros</p>'
+    })
+    console.log('3')
+}
+
+
